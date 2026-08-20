84a283cff186f1211a458abb6ede9f2e1950c84d
pandaass <saggindas@gmail.com>
2026-08-20 17:11:22 +0800

    Added List

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -6,7 +6,12 @@ import java.util.Scanner;
 public class Bob {
     /** Separates each chatbot response in the terminal. */
     private static final String DIVIDER = "____________________________________________________________";
+    private static final String BLUE = "\u001B[34m";
+    private static final String GREEN = "\u001B[32m";
+    private static final String RESET = "\u001B[0m";
 
+    private static String[] list = new String[100];
+    private static int currListLen = 0;
     /**
      * Displays Bob's welcome banner and responds to commands until the user says bye.
      *
@@ -20,21 +25,37 @@ public class Bob {
                 + "|____/ \\___/|_.__/ \n";
 
         System.out.println(banner);
-        System.out.println("Hello! I'm Bob.");
-        System.out.println("What can I do for you?");
+        System.out.println(GREEN + "Hello! I'm Bob.");
+        System.out.println("What can I do for you?" + RESET);
         System.out.println(DIVIDER);
+        System.out.println();
 
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNextLine()) {
+            System.out.println(DIVIDER);
+            System.out.println();
             String command = scanner.nextLine();
             if (command.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(BLUE + "Bye. Hope to see you again soon!" + RESET);
                 System.out.println(DIVIDER);
                 break;
             }
-
-            System.out.println(command);
+            if (command.equals("list")) {
+                if (currListLen == 0) {
+                    System.out.println(GREEN + "Nothing in list yet!" + RESET);
+                    continue;
+                }
+                for (int i = 0; i < currListLen; i++) {
+                    System.out.println(GREEN + (i+1) + ". " + list[i] + RESET);
+                }
+            } else {
+                list[currListLen] = command;
+                currListLen++;
+    
+                System.out.println(GREEN + command + RESET);
+            }
             System.out.println(DIVIDER);
+            System.out.println();
         }
     }
 }
